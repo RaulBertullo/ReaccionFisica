43acb1a2a0a155d3b7fb96172c5da2f55582807c
RaulBertullo <rabeal@alumnatflorida.es>
2026-04-14 11:12:27 +0200

    PLANETA CREADO

--- reaccionFisica/Assets/CohetePlaneta.cs
+++ reaccionFisica/Assets/CohetePlaneta.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 
-public class CoheteFuerza : MonoBehaviour
+public class CohetePlaneta : MonoBehaviour
 {
     Rigidbody rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(Vector3.up * 10f);
+        
     }
 }
